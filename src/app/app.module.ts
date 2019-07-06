fc3c73c41cd9b5fda44bc136409de6d8bb3fb4c6
Citlalli Prado <citlalli.prado@gmail.com>
2019-07-06 18:14:54 +0200

    Created project and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ContactComponent } from './contact/contact.component';
+import { UserMenuComponent } from './user-menu/user-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TaskListComponent,
+    CreateTaskComponent,
+    WelcomeComponent,
+    ContactComponent,
+    UserMenuComponent
   ],
   imports: [
     BrowserModule,
